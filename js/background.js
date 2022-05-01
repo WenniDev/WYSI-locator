39ac4b689167ff2d76be32a735b4f28b0c8e7224
Whenever <whenever.fansub@gmail.com>
2022-05-01 19:51:10 +0200

    little fix

--- js/background.js
+++ js/background.js
@@ -1,8 +1,7 @@
 (function() {
     'use strict';
 
-    let regex727;
-    regex727 = new RegExp(/7[.\/\-_|()"'~}{\[\]:;,+=*<>! ]?2[.\/_\-|()"'~}{\[\]:;,+=*<>! ]?7/, "g");
+    let regex727 = new RegExp(/7[.\/\-_|()"'~}{\[\]:;,+=*<>! ]?2[.\/_\-|()"'~}{\[\]:;,+=*<>! ]?7/, "g");
 
     function find727(element, regex) {
         return element.innerText.match(regex) !== null;
@@ -11,10 +10,8 @@
     function is727() {
         const body = document.querySelector("body");
 
-        console.log(find727(body, regex727));
         if (!find727(body, regex727)) return;
         console.log("Found!");
-        chrome
         alert("WHEN YOU SEE IT!");
         return;
 
